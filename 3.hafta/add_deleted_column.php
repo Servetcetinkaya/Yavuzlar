339067e44426537fcb109cd4858f3c3e9d8b20ac
Servet Çetinkaya <153910390+Servetcetinkaya@users.noreply.github.com>
2024-09-28 21:59:57 +0300

    Update add_deleted_column.php

--- 3.hafta/add_deleted_column.php
+++ 3.hafta/add_deleted_column.php
@@ -2,11 +2,11 @@
 $database_path = "/tmp/yemek_sitesi.db";
 
 try {
-    // Veritabanı bağlantısı oluşturuluyor
+   
     $connection = new PDO("sqlite:$database_path");
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // users tablosuna deleted sütunu ekleme
+   
     $connection->exec("ALTER TABLE users ADD COLUMN deleted INTEGER DEFAULT 0");
 
     echo "Deleted sütunu başarıyla eklendi.";
